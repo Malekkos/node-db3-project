c1019187a0afdb6734183770fa698f07d60be160
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-28 11:53:21 -0800

    began work on first middleware, checkschemeid

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -7,7 +7,12 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
+  const id = req.params.id
+  if (!id) {
+    res.status(404).json({
+      message: `scheme with scheme_id ${id} not found`
+    })
+  }
 }
 
 /*
